278f82e3db11257edb8571b8e5001e1ae6769b9a
Nour2610 <nour.tarek2610@gmail.com>
2014-12-09 22:06:11 +0200

    Register Table implementation

---
+++ Tomasulo/src/components/RegisterTable.java
@@ -0,0 +1,28 @@
+package components;
+
+import java.util.HashMap;
+
+public class RegisterTable {
+
+	RegisterFile regFile;
+	HashMap<Register, ReservationStation> registerTable;
+
+	public RegisterTable() {
+		registerTable.put(regFile.R1, null);
+		registerTable.put(regFile.R2, null);
+		registerTable.put(regFile.R3, null);
+		registerTable.put(regFile.R4, null);
+		registerTable.put(regFile.R5, null);
+		registerTable.put(regFile.R6, null);
+		registerTable.put(regFile.R7, null);
+	}
+
+	public void setRSEntry(Register r, ReservationStation rs) {
+		registerTable.put(r, rs);
+	}
+
+	public ReservationStation getRSEntry(Register r) {
+		return registerTable.get(r);
+	}
+
+}
