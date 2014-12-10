980f998b6a8ee59ac25af0863683348c81bb0b0e
Nour2610 <nour.tarek2610@gmail.com>
2014-12-10 05:48:13 +0200

    Adding common data bus class

--- Tomasulo/src/components/CommonDataBus.java
+++ Tomasulo/src/components/CommonDataBus.java
@@ -1,9 +1,41 @@
 package components;
 
 public class CommonDataBus {
+	// Each piece of data on the bus has a source FU represented by the RS
+	// and has a destination Register that it needs to be written to
+	ReservationStation source;
+	String data;
+	Register destination;
 
-	public CommonDataBus() {
-		// TODO Auto-generated constructor stub
+	public CommonDataBus(ReservationStation source, String data,
+			Register destination) {
+		this.source = source;
+		this.data = data;
+		this.destination = destination;
+	}
+
+	public ReservationStation getSource() {
+		return source;
+	}
+	// in write phase?
+	public void setSource(ReservationStation source) {
+		this.source = source;
+	}
+
+	public String getData() {
+		return data;
+	}
+	// in write phase?
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Register getDestination() {
+		return destination;
+	}
+	// in write phase?
+	public void setDestination(Register destination) {
+		this.destination = destination;
 	}
 
 }
